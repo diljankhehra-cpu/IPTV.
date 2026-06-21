623650f68de6113774c3608737dbaedf54d93865
diljankhehra-cpu <diljankhehra@gmail.com>
2026-06-21 07:50:58 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 import requests
+import os
 
 def update_list():
-    # ਉਹ ਸਾਰੀਆਂ ਫਾਈਲਾਂ ਜੋ ਤੈਨੂੰ ਚਾਹੀਦੀਆਂ ਹਨ
     files = ["in", "in_distro", "in_doordarshan", "in_pishow", "in_samsung", "in_tango"]
     
     with open("in.m3u", "w", encoding="utf-8") as outfile:
@@ -11,10 +11,13 @@ def update_list():
             url = f"https://iptv-org.github.io/iptv/languages/{file_name}.m3u"
             response = requests.get(url)
             if response.status_code == 200:
-                # ਫਾਈਲ ਵਿੱਚੋਂ ਪਹਿਲੀ ਲਾਈਨ (#EXTM3U) ਹਟਾ ਕੇ ਬਾਕੀ ਡੇਟਾ ਜੋੜੋ
                 lines = response.text.splitlines()
                 for line in lines[1:]: # ਪਹਿਲੀ ਲਾਈਨ ਛੱਡ ਕੇ
                     if line.strip():
                         outfile.write(line + "\n")
 
-update_list()
+    # ਇਹ ਯਕੀਨੀ ਬਣਾਉਣ ਲਈ ਕਿ git ਨੂੰ ਲੱਗੇ ਕਿ ਕੁਝ ਬਦਲਿਆ ਹੈ
+    os.system("echo ' ' >> in.m3u") 
+
+if __name__ == "__main__":
+    update_list()
